e2ba888daa7e874eb86c72c6d9e51d7751c667b5
Hussain Tinwala <coder.hussaintin@gmail.com>
2025-03-02 15:26:40 +0530

    fetch recipe details based on dynamic id and display them in Details component

--- src/pages/Details.jsx
+++ src/pages/Details.jsx
@@ -1,7 +1,46 @@
-import React from 'react'
+import React, { useContext, useEffect } from 'react'
+import { useParams } from 'react-router-dom'
+import { GlobalContext } from '../context/context'
 
 export default function Details() {
+  const { id } = useParams()
+  const { recipeDetailsData, setRecipeDetailsData } = useContext(GlobalContext)
+  // console.log(params)
+
+  useEffect(() => {
+    async function getRecipeDetails() {
+      try {
+        const res = await fetch(`https://forkify-api.herokuapp.com/api/v2/recipes/${id}`)
+        const data = await res.json()
+        console.log(data)
+
+        if(data?.data)
+        {
+          setRecipeDetailsData(data?.data)
+        }
+
+      } catch (error) {
+        console.log("Error fetching data: " + error)
+
+      }
+    }
+    getRecipeDetails()
+  }, [])
+
   return (
-    <div>Details</div>
+    <div className='container mx-auto py-10 grid grid-cols-1 lg:grid-cols-2 gap-10'>
+      <div className="row-start-2 lg:row-start-auto">
+        <div className="h-96 overflow-hidden rounded-xl group">
+          <img src={recipeDetailsData?.recipe?.image_url} alt="Recipe Item" className='w-full h-full object-cover block group-hover:scale-105 duration-300'/>
+        </div>
+      </div>
+      <div className='flex flex-col gap-3'>
+      <span className="text-sm text-cyan-700 font-medium">{recipeDetailsData?.recipe?.publisher}</span>
+      <h3 className="font-bold truncate text-black">{recipeDetailsData?.recipe?.title}</h3>
+      
+
+      </div>
+
+    </div>
   )
 }
