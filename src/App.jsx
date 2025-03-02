6dd66493068516275356431190c3e1c9761ae4d1
Hussain Tinwala <coder.hussaintin@gmail.com>
2025-03-02 13:53:03 +0530

    Add pages and components and integrate using react-router

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,36 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import { Routes } from 'react-router-dom'
+import { Route } from 'react-router-dom'
+import NavBar from './components/NavBar'
+import Home from './pages/Home'
+import Favourites from './pages/Favourites'
+import Details from './pages/Details'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
+    <div>
+      <div className="min-h-screen p-6 bg-white text-gray-600 text-lg">
+        <NavBar />
+        <Routes>
+          <Route 
+            path='/'
+            element={ <Home/>}
+          />
+          <Route 
+            path='/favourites'
+            element={ <Favourites/>}
+          />
+          <Route 
+            path='/recipe-item/:id'
+            element={ <Details/>}
+          />
+        </Routes>
+      </div>
+    </div>
       
     </>
   )
