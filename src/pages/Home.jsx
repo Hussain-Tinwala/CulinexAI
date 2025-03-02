31f7581bc8e965441198615aa14e825e4807366e
Hussain Tinwala <coder.hussaintin@gmail.com>
2025-03-02 15:25:51 +0530

    display loading state and conditionally render recipes in Home component

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,8 +1,28 @@
-import React from 'react'
+import React, { useContext } from 'react'
+import { GlobalContext } from '../context/context'
+import RecipeItem from '../components/RecipeItem'
 
 function Home() {
+  const { recipeList, loading } = useContext(GlobalContext)
+
+  if(loading) return <div>Loading... Please wait</div>
+
   return (
-    <div>Home</div>
+    <div className='py-8 container mx-auto flex flex-wrap justify-center gap-10'>
+      {recipeList && recipeList.length > 0 ?
+        recipeList.map((item) => {
+          return (
+            <RecipeItem item={item} />
+          )
+        })
+        : <div>
+          <p className='lg:text-4xl text-xl text-center text-black font-extrabold'>
+            Nothing to show here
+          </p>
+        </div>
+      }
+
+    </div>
   )
 }
 
